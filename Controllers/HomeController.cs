59ff5c9ab06f46e052cac0503536319b3ce66a68
Ricardo <ricardoraf2326@gmail.com>
2022-02-27 00:08:40 -0300

    Atividade atualizada

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Biblioteca.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Autenticacao.CheckLogin(this);
+            ViewData["mensagem"] = "seja bem vindo ao sistema de controle de empréstimo de livros";
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string login, string senha)
+        {
+            if (Autenticacao.verificaLoginSenha(login, senha, this))
+            {
+                return RedirectToAction("Index");
+
+            }/*
+            if (login != "admin" || senha != "123") */
+            else
+            {
+                ViewData["Erro"] = "Senha inválida";
+                return View();
+            }
+            /*else
+            {
+                HttpContext.Session.SetString("user", "admin");
+                return RedirectToAction("Index");
+            }*/
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
